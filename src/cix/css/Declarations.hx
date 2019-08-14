2f89ec927b0de2f9fba328dd5802aedac68707de
Juraj Kirchheim <back2dos@googlemail.com>
2019-08-14 18:15:12 +0200

    Resolve runtime initialization order issues.

--- src/cix/css/Declarations.hx
+++ src/cix/css/Declarations.hx
@@ -3,8 +3,10 @@ package cix.css;
 import tink.domspec.ClassName;
 
 class Declarations {
-  static var declared = new Map();
+  static var declared:Map<String, Bool>;
   static public function add(className:ClassName, css:()->String) {
+    if (declared == null)
+      declared = new Map();
     if (!declared[className]) {
       Runtime.addRule(className, css());
       declared[className] = true;
--- src/cix/css/Runtime.hx
+++ src/cix/css/Runtime.hx
@@ -2,20 +2,26 @@ package cix.css;
 
 #if (js && !nodejs)
 class Runtime {
-  static var indices = new Map();
-  static var sheet:js.html.CSSStyleSheet = {
-    var style = js.Browser.document.createStyleElement();
-    js.Browser.document.head.appendChild(style);
-    cast style.sheet;
-  }
-  static public function addRule(id:String, css:String) 
+  static var indices:Map<String, Int>;
+  
+  static var sheet:js.html.CSSStyleSheet;
+  static public function addRule(id:String, css:String) {
+    if (indices == null) {
+      indices = new Map();
+      
+      var style = js.Browser.document.createStyleElement();
+      js.Browser.document.head.appendChild(style);
+      sheet = cast style.sheet;
+    }
+
     sheet.insertRule(
-      css, 
+      '@media all { $css }', 
       switch indices[id] {
         case null: indices[id] = sheet.cssRules.length;
         case v: v;
       }
     );
+  }
 }
 #else
 @:notSupported class Runtime {
