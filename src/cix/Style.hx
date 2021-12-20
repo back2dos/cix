400e589e639985e4b0ad7fb62006aa217d3b5858
Juraj Kirchheim <back2dos@gmail.com>
2021-12-20 10:10:09 +0100

    Add Style.styled.

--- src/cix/Style.hx
+++ src/cix/Style.hx
@@ -2,10 +2,13 @@ package cix;
 
 class Style {
 
-  macro static public function rule(e) 
+  macro static public function rule(e)
     return cix.css.Generator.makeRule(e);
 
-  macro static public function sheet(e) 
+  macro static public function sheet(e)
     return cix.css.Generator.makeSheet(e);
+
+  macro static public function styled(target, e)
+    return macro ${cix.css.Generator.makeRule(e)}.applyTo($target);
 
 }
